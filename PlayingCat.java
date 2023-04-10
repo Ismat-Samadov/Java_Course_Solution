454e43d7fe1cf6e8b69a86ba1173144bbd0d0a26
Ismat-Samadov <ismetsemedov@gmail.com>
2023-04-10 11:10:18 +0400

    iscatplaying

--- PlayingCat.java
+++ PlayingCat.java
@@ -1,5 +1,6 @@
 //
-//The cats spend most of the day playing. In particular, they play if the temperature is
+//The cats spend most of the day playing.
+// In particular, they play if the temperature is
 // between 25 and 35 (inclusive). Unless it is summer,
 // then the upper limit is 45 (inclusive) instead of 35.
 //
@@ -32,8 +33,16 @@
 //        Do not add the main method to the solution code.
 
 
-
 public class PlayingCat {
 
-
-}
+    public static boolean isCatPlaying(boolean summer,int temperature){
+        boolean result=false ;
+        if(summer == true && (temperature > 24 && temperature <= 45)){
+            result= true;
+        }
+        else if (summer ==false &&(temperature>24 && temperature<36)){
+            result= true;
+        }
+        return result;
+    }
+    }
