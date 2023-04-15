a055187b0d0eeea58844036571e4ab0b66bbbc29
Ismat-Samadov <ismetsemedov@gmail.com>
2023-04-15 23:00:31 +0400

    factorsOfNumber

--- FactorPrinter.java
+++ FactorPrinter.java
@@ -42,11 +42,18 @@
 
 public class FactorPrinter {
 
-    public static void printFactors(int number){
-        if (number>0){
+    public static void printFactors(int number) {
+        if (number > 0) {
 
+            for (int i = 1; i <= number; ++i) {
+
+                if (number % i == 0) {
+                    System.out.println(i);
+                }
+            }
+
+        } else {
+            System.out.println("Invalid Value");
         }
-        else {System.out.println("Invalid Value");}
-
     }
 }
