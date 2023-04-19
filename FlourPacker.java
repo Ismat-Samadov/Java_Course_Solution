83f7dd4a5e2d9c930432dbde9624f8a7f8ed5b78
Ismat-Samadov <ismetsemedov@gmail.com>
2023-04-19 21:00:56 +0400

    save

--- FlourPacker.java
+++ FlourPacker.java
@@ -1,16 +1,12 @@
 // Write a method named canPack with three parameters of type int named bigCount, smallCount,
-// and goal. 
-
+// and goal.
 // The parameter bigCount represents the count of big flour bags (5 kilos each).
-
 // The parameter smallCount represents the count of small flour bags (1 kilo each).
-
 // The parameter goal represents the goal amount of kilos of flour needed to assemble a package.
-
 // Therefore, the sum of the kilos of bigCount and smallCount must be at least equal to the 
-//  value of goal. The method should return true if it is possible to make a package with 
+//  value of goal.
+//  The method should return true if it is possible to make a package with
 //  goal kilos of flour.
-
 // If the sum is greater than goal, ensure that only full bags are used towards the goal amount. 
 // For example, if goal = 9, bigCount = 2, and smallCount = 0, the method should return false 
 // since each big bag is 5 kilos and cannot be divided. However, if goal = 9, bigCount = 1, 
@@ -18,7 +14,6 @@
 //  smallCount bags equal goal, and it's okay if there are additional bags left over.
 
 // If any of the parameters are negative, return false.
-
 
 
 // EXAMPLE INPUT/OUTPUT:
@@ -38,13 +33,22 @@
 // canPack (-3, 2, 12); should return false since bigCount is negative.
 
 
-
-// NOTE: The method canPack should be defined as public static like we have been doing so far 
+// NOTE: The method canPack should be defined as public static like we have been doing so far
 // in the course.
 
 // NOTE: Do not add a main method to the solution code.
 
 public class FlourPacker {
 
-
+    public static boolean canPack(int bigCount, int smallCount, int goal) {
+        boolean result = false;
+        if (bigCount >= 0 && smallCount >= 0 && goal >= 0) {
+            if (goal >= (bigCount + smallCount)) {
+                result = true;
+            }
+        } else {
+            result = false;
+        }
+        return result;
+    }
 }
