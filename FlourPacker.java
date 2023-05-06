94365d17be203a79658366324493649f049cbfe0
Səmədov İsmət <ismetsemedov@gmail.com>
2023-05-06 16:30:38 +0400

    calc

--- FlourPacker.java
+++ FlourPacker.java
@@ -42,24 +42,23 @@
 public class FlourPacker {
 
 
-    public static boolean canPack(int bigCount, int smallCount, int goal){
-        boolean status=false;
-        int totalWeight = (bigCount*5)+smallCount;    
+    public static boolean canPack(int bigCount, int smallCount, int goal) {
+        boolean status = false;
+        int totalWeight = (bigCount * 5) + smallCount;
 
-        if(bigCount<0 || smallCount<0 || goal<0){
-            status=false;
-        }else if(totalWeight<goal){
-            status=false;
-        }
-        else if(smallCount >= goal){
-            status=true;
-        }else if(goal>=5 && goal%5==0 && goal<=bigCount*5){
-            status=true;
-        }else if((goal-(bigCount*5))<=smallCount && (goal-(bigCount*5))>0 ){
-            status =true;
-        }else if((goal/5)<=bigCount && (goal%5)<=smallCount){
-            status=true;
+        if (bigCount < 0 || smallCount < 0 || goal < 0) {
+            status = false;
+        } else if (totalWeight < goal) {
+            status = false;
+        } else if (smallCount >= goal) {
+            status = true;
+        } else if (goal >= 5 && goal % 5 == 0 && goal <= bigCount * 5) {
+            status = true;
+        } else if ((goal - (bigCount * 5)) <= smallCount && (goal - (bigCount * 5)) > 0) {
+            status = true;
+        } else if ((goal / 5) <= bigCount && (goal % 5) <= smallCount) {
+            status = true;
         }
         return status;
     }
-    }
+}
