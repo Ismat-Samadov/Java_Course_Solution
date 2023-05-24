f9077e47ca4a28ba4bb0ae9b08c609676d563867
Ismat-Samadov <ismetsemedov@gmail.com>
2023-05-24 16:56:51 +0400

    check

--- NumberGuessing.java
+++ NumberGuessing.java
@@ -1,28 +1,19 @@
 import java.lang.Math;
-
 import java.util.Scanner;
 
 public class NumberGuessing {
-
     static Scanner scan = new Scanner(System.in);
-
     public static void game() {
         String user_input = scan.next();
-
-
     }
-
     public static void exit() {
         System.out.println("Press 0 for quit");
     }
-
     public static void winner() {
         System.out.println("Congrats man !");
         System.out.println("You are winner !!!");
-
     }
-
     public static int rng() {
-        return (int) (Math.random() * 1000);
+        return (int) (Math.random() * 100);
     }
 }
