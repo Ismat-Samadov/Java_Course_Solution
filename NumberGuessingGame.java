04bedc39ccd558c37ce355f2614bfd5de4b63eb5
Səmədov İsmət <ismetsemedov@gmail.com>
2023-05-27 20:18:34 +0400

    check

--- NumberGuessingGame.java
+++ NumberGuessingGame.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class NumberGuessing {
+public class NumberGuessingGame {
     public static void main(String[] args) {
         System.out.println("Welcome to the Number Guessing Game!");
 
